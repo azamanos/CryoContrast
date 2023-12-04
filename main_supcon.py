23290c10e3de70856194a336e5acae29cd062209
lobracost <pa.koromilas@gmail.com>
2023-12-04 13:00:57 +0200

    minor

--- main_supcon.py
+++ main_supcon.py
@@ -244,7 +244,7 @@ def train(train_loader, model, criterion, optimizer, epoch, opt):
 
         # update metric
         losses.update(loss.item(), bsz)
-
+        print(loss.item())
         # SGD
         optimizer.zero_grad()
         loss.backward()
