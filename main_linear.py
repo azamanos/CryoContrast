c3adad1e070d8e5117bb0ed0e14b3819a0b1cb46
lobracost <pa.koromilas@gmail.com>
2023-12-05 12:42:08 +0200

    minor

--- main_linear.py
+++ main_linear.py
@@ -165,7 +165,7 @@ def train(train_loader, model, classifier, criterion, optimizer, epoch, opt):
         # update metric
         losses.update(loss.item(), bsz)
         if opt.n_cls < 10:
-            acc1 = accuracy(output, labels, topk=1)
+            acc1 = accuracy(output, labels)
         else:
             acc1, acc5 = accuracy(output, labels, topk=(1, 5))
         top1.update(acc1[0], bsz)
